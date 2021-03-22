f7e4df4ce402549aa4173364fe64d09a0ef0fba1
Gleibson Alves <alvesgleibson@gmail.com>
2021-03-21 23:02:41 -0300

    Adicionei o IF

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,13 @@ package application;
 public class Main {
 
 	public static void main(String[] args) {
-			System.out.println("Ola Mundo");
+		System.out.println("Ola Mundo");
+
+		int a = 10;
+
+		if (a == 10) {
+			System.out.println("Voce Passou");
+		}else System.out.println("Não Passou");
 	}
 
 }
